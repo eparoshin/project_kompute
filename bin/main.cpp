7701750aca5d14c6c2ab6a3675056f31405cbc65
Egor Paroshin <eparoshin@gmail.com>
2024-01-20 21:16:53 +0300

    init project

---
+++ bin/main.cpp
@@ -0,0 +1,5 @@
+#include <lib/test.h>
+
+int main() {
+    func();
+}
---
+++ lib/test.cpp
@@ -0,0 +1,8 @@
+#include "test.h"
+#include <iostream>
+
+#include <kompute/Kompute.hpp>
+
+void func() {
+    std::cout << "hello" << std::endl;
+}
