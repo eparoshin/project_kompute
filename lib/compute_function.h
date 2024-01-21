c2d782efbf42970fec5a61e1c47f6b8629f1eb5a
Egor Paroshin <eparoshin@gmail.com>
2024-01-21 17:42:48 +0300

    new files

---
+++ lib/compute_function.h
@@ -0,0 +1,64 @@
+#pragma once
+
+#include <memory>
+#include <type_traits>
+#include <vector>
+#include <functional>
+#include <algorithm>
+
+#include <kompute/Algorithm.hpp>
+#include <kompute/Tensor.hpp>
+#include <kompute/operations/OpAlgoDispatch.hpp>
+
+namespace NComputeFunctions {
+using TDataType = float;
+using TTensorPtr = std::shared_ptr<kp::Tensor>;
+using TSpirvProgramm = std::vector<uint32_t>;
+using TSpirvProgrammConstRef = const TSpirvProgramm&;
+using TSpirvGetFunction = std::function<TSpirvProgrammConstRef()>;
+using TTensorsVec = std::vector<TTensorPtr>;
+using TAlgorithmPtr = std::shared_ptr<kp::Algorithm>;
+
+class TPlotComputeFunction : public kp::OpAlgoDispatch {
+    using TVectorD = std::vector<double>;
+
+    TPlotComputeFunction(TTensorPtr&& means, TTensorPtr&& args, TTensorPtr&& out,
+                         TAlgorithmPtr&& algorithm, TSpirvGetFunction getSpirv, TVectorD* outVec)
+        : OpAlgoDispatch(algorithm)
+        , OutTensor(out)
+        , OutVec(outVec) {
+        assert(dynamic_cast<kp::TensorT<TDataType>*>(means.get()) &&
+               "means tensor has incorrect type");
+        assert(dynamic_cast<kp::TensorT<TDataType>*>(args.get()) &&
+               "args tensor has incorrect type");
+        assert(dynamic_cast<kp::TensorT<TDataType>*>(out.get()) &&
+               "out tensor has incorrect type");
+
+        assert(args->size() == out->size() && "sizes mismatch");
+        assert(outVec);
+        assert(outVec->size() == args->size() && "out vector wrong size");
+
+        algorithm->rebuild<>({means, args, out}, getSpirv());
+    }
+
+   public:
+    template <typename TSpirvGetter>
+    static std::shared_ptr<TPlotComputeFunction> CreateComputeFunction(TTensorPtr means, TTensorPtr args, TTensorPtr out,
+                         TAlgorithmPtr algorithm, TVectorD* outVec) {
+        return std::make_shared<TPlotComputeFunction>(std::move(means), std::move(args), std::move(out), algorithm, TSpirvGetter::Get, outVec);
+    }
+    TTensorPtr GetOutTensor() {
+        return OutTensor;
+    }
+
+    void FillOutVec() {
+        auto* fromBegin = OutTensor->data<TDataType>();
+        auto* fromEnd = fromBegin + OutTensor->size();
+        std::copy(fromBegin, fromEnd, OutVec->begin());
+    }
+
+   private:
+    TTensorPtr OutTensor;
+    TVectorD* OutVec;
+};
+}
